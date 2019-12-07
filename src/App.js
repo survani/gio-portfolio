b2a49acbe1ff2b373d9bfa4967a7ae251548673f
Giovani Garfias <46204471+survani@users.noreply.github.com>
2019-12-06 19:43:38 -0800

    Added simple Footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route } from 'react-router-dom';
 import './sass/styles.scss';
 import Navigation from './Components/Navigation';
 import Home from './Components/Home.jsx';
+import Footer from './Components/footer';
 
 function App() {
   return (
     <div className="App">
       <Navigation/> 
       <Route exact path='/' component={Home}/>
+      <Footer/>
       
     </div>
   );
---
+++ src/Components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <diV className='footer-container'>
+            <p>Alpha version 0.1</p>
+        </diV>
+    )
+}
+
+export default Footer;
