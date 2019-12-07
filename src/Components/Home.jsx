47acf909bc92c1c8bf3ef910d427541f2397b801
Giovani Garfias <46204471+survani@users.noreply.github.com>
2019-12-06 17:18:15 -0800

    Fixed Issue with git

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 const Home = () => {
     return (
-<<<<<<< HEAD
         <div className='home-bio-container'>
             <div className='bio-desc-container'>
                 <div className='bio-img'>
@@ -17,20 +16,6 @@ const Home = () => {
                         <a href='https://twitter.com/garfias_giovani'>Twitter</a>
                         <a href='https://dev.to/garfias_giovani'>Dev</a>
                     </div>
-=======
-        <div className='main-container'>
-            <div className='home-bio-container'>
-                <div className='bio-img'>
-                    <img src='https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' />
-                </div>
-                <div className='bio-title'>
-                    <h1>Hello! I am Giovani a Full Stack Web Developer :)</h1>
-                </div>
-                <div className='bio-links'>
-                    <a href='https://github.com/survani'>GitHub</a>
-                    <a href='https://github.com/survani'>Twitter</a>
-                    <a href='https://github.com/survani'>Dev</a>
->>>>>>> 12315f28f9c375632ac728fd158a94cf00a6d433
                 </div>
             </div>
         </div>
