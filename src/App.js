e3be888b8f258f327c44c1907eff2da83afe65aa
Giovani Garfias <46204471+survani@users.noreply.github.com>
2019-12-06 23:03:44 -0800

    Added Project Page Styling and Content

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import './sass/styles.scss';
+
 import Navigation from './Components/Navigation';
 import Home from './Components/Home.jsx';
-import Footer from './Components/footer';
+// import Footer from './Components/Footer';
+import Projects from './Components/Projects';
 
 function App() {
   return (
     <div className="App">
       <Navigation/> 
       <Route exact path='/' component={Home}/>
-      <Footer/>
+      <Route path='/projects' component={Projects}/>
       
     </div>
   );
