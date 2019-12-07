6241bd0257bc79e636afc6d5a38e7091335406e1
Giovani Garfias <46204471+survani@users.noreply.github.com>
2019-12-06 23:20:22 -0800

    Fixed Bug on Project Page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-            <p>ABOUT ME COMING SOON!</p>
+            <p className='about-text'>ABOUT ME COMING SOON!</p>
         </div>
     )
 }
