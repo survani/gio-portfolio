82c91f6806119c4488e398ed8efd2181b8b0f587
Giovani Garfias <46204471+survani@users.noreply.github.com>
2019-12-06 17:18:40 -0800

    last fix

--- src/index.js
+++ src/index.js
@@ -1,31 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-=======
-import { BrowserRouter as Router } from 'react-router-dom'
-
-import './index.css';
->>>>>>> 12315f28f9c375632ac728fd158a94cf00a6d433
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {
     BrowserRouter as Router,
 } from 'react-router-dom'
 
-<<<<<<< HEAD
 
 ReactDOM.render(
 <Router>
     <App />
 </Router>, 
 document.getElementById('root'));
-=======
-ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
-    document.getElementById('root'));
->>>>>>> 12315f28f9c375632ac728fd158a94cf00a6d433
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
