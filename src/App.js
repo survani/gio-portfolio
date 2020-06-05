b2e3b9b25f42a32e01ca434df51fe7c9b0fce79d
Giovani Garfias <46204471+survani@users.noreply.github.com>
2020-06-04 17:01:56 -0700

    Added Under Construction Message + Upgrading the site.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePage from './components/homepage/homepage';
+import UnderConstruction from './containers/underconstruction/UnderConstruction';
+function App() {
+  return (
+    <>
+      <UnderConstruction />
+    </>
+  );
+}
+
+export default App;
---
+++ src/containers/aboutme/style.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const AboutMeSection = styled.section`
+  background: #c4cadd;
+`;
+
+export const AboutTitle = styled.section`
+  color: white;
+  text-align: center;
+  font-size: 30px;
+  font-family: Montserrat;
+  font-weight: 700;
+  margin-top: 2rem;
+`;
+
+export const AboutImage = styled.img`
+  width: 800px;
+`;
+
+export const AboutContent = styled.h2`
+  font-family: Montserrat;
+  font-size: 18px;
+  display: flex;
+  margin-top: 2rem;
+`;
+
+export const AboutHeader = styled.h2`
+  margin-top: 5rem;
+`;
